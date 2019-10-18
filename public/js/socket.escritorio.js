dddfb5e97949952b3d86e224077f99dd624fb5fb
FernandoCM18 <fernando.c.m@live.com>
2019-10-18 12:17:55 -0700

    Primer commit -Produccion

---
+++ public/js/socket.escritorio.js
@@ -0,0 +1,29 @@
+// Comando para establecer la conexion
+var socket = io();
+
+var searchParams = new URLSearchParams( window.location.search );
+var label = $('small');
+
+if ( !searchParams.has('escritorio') ) {
+    window.location = 'index.html'; 
+    throw new Error('El escritorio es necesario');
+}
+
+var escritorio = searchParams.get('escritorio');
+console.log(escritorio);
+
+$('h1').text('Escritorio ' + escritorio);
+
+$('button').on('click', function() {
+    
+    socket.emit('atenderTicket', { escritorio: escritorio }, function(resp) {
+
+        if (resp === 'No hay tickets') {
+            label.text(resp);
+            alert(resp);
+            return;
+        }
+        
+        label.text('Ticket ' + resp.numero);
+    });
+});
---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,27 @@
+// Comando para establecer la conexion
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+    
+});
+
+socket.on('disconnect', function() {
+    console.log('Desconectado del servidor ');
+});
+
+socket.on('estadoActual', function(resp) {
+    console.log(resp);
+    label.text( resp.actual );
+});
+
+$('button').on('click', function(){
+
+    socket.emit('siguienteTicket', null, function(siguienteTicket){
+
+        label.text(siguienteTicket);
+
+    });
+});
